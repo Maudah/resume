519aa0524933e244b4716dc10089cd0da88c5302
LITAL <lital@upstream.auto>
2020-11-06 13:08:01 +0200

    Home page

--- src/component/slidebar/index.js
+++ src/component/slidebar/index.js
@@ -4,19 +4,25 @@ import './style.css';
 
 import home from '../../images/house-32.ico'
 import about from '../../images/about-32.ico'
+import github from '../../images/github-10-32.ico'
+import linkedin from '../../images/linkedin-3-32.ico'
 
 class SideBar extends Component {
 
   render() {
     return (
       <div className='slide-bar'>
-        <div>
           <div style={{padding: '12px'}}>
-            <Link style={{ textDecoration: 'none' }} to='/'><img src={home} alt="Logo" /></Link>
+            <Link style={{ textDecoration: 'none' }} to='/'><img src={home} alt="home" /></Link>
           </div>
           <div style={{padding: '12px'}}>
-            <Link style={{ textDecoration: 'none' }} to='/about'><img src={about} alt="Logo" /></Link>
+            <Link style={{ textDecoration: 'none' }} to='/about'><img src={about} alt="about" /></Link>
           </div>
+          <div style={{padding: '12px'}}>
+            <Link style={{ textDecoration: 'none' }} to='/github'><img src={github} alt="github" /></Link>
+          </div>
+          <div style={{padding: '12px'}}>
+          <Link style={{ textDecoration: 'none' }} to='/linkdin'><img src={linkedin} alt="likdin" /></Link>
         </div>
       </div>
     );
--- src/screen/Home/index.js
+++ src/screen/Home/index.js
@@ -12,6 +12,13 @@ export default class Home extends Component {
         <video className='home-page-video' autoPlay loop={true} muted={true} data-reactid=".0.1.0.0">
           <source type="video/mp4" data-reactid=".0.1.0.0.0" src={startVideo} />
         </video>
+
+        <div className="home-page-text-content">
+          <h1 id='slide'>Hi, </h1>
+          <h1 id='slide1'>I'm Lital </h1>
+          <h2 id='slide2'>Infrastructures and DevOps engineer at CEVA .Inc</h2>
+          <p id='element'>C++ | Python | C | HTML | CSS | React-Native</p>
+        </div>
       </div>
     );
   }
